3ad1cde6071e383a205aeee38be1def5b01159e9
PietREKT <piotr_stoinski02@wp.pl>
2024-12-07 17:49:11 +0100

    config changes3

--- src/app/myRxStompConfig.ts
+++ src/app/myRxStompConfig.ts
@@ -1,7 +1,7 @@
 import {RxStompConfig} from "@stomp/rx-stomp";
 
 export const myRxStompConfig : RxStompConfig = {
-  brokerURL: 'wss://2a76-2a02-a315-41d3-4800-3477-eafa-672b-6b27.ngrok-free.app/ws',
+  brokerURL: 'https://alkokarty.onrender.com/ws',
 
   connectHeaders: {},
 
